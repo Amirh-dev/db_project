6d9545f01656a99063c7c4008ef339eff2270214
amir <amirbagheri322@yahoo.com>
2024-01-29 23:13:54 +0330

    add routing

--- pages/seller.js
+++ pages/seller.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const SellerPage = () => {
   return (
     <div>
       <h1>Seller Page</h1>
-      {/* Add seller-specific content */}
+      <nav>
+        <ul>
+          <li>
+            <Link href="/seller/activeRentals">
+              <button>Active Rentals</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/customerList">
+              <button>Customer List</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/list-movies-by-genre">
+              <button>List Movies by Genre</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/movieList">
+              <button>Movie List</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/MovieSearch">
+              <button>Movie Search</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/rental">
+              <button>Rental</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/reserve">
+              <button>Reserve</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/storeInfo">
+              <button>Store Info</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/seller/totalPayments">
+              <button>Total Payments</button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
